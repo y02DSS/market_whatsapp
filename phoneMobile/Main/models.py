c9a8de5a4c8b0563cc1166e872dec62e08f6a060
y02DSS <57633706+y02DSS@users.noreply.github.com>
2022-10-03 03:38:48 +0300

    Что-то починил с фотками

--- phoneMobile/Main/models.py
+++ phoneMobile/Main/models.py
@@ -4,8 +4,9 @@ from jsonfield import JSONField
 def default_specific_workouts():
     return {"posts": []}
     
-class MainAdmin(models.Model):
+class MainAdmin(models.Model):               
     name = models.CharField(max_length=200)
+    group_home = models.CharField(max_length=200, blank=True, null=True)
     info = JSONField(default = default_specific_workouts())
 
 
